34797a044050eb11c4e153fc65be4b45ce3d9b98
adit <arafatislamadit2026.com>
2026-09-06 20:31:30 +0600

    Adding Home Route

--- index.js
+++ index.js
@@ -2,7 +2,32 @@
 
 const http = require("http")
 const server = http.createServer((req, res) =>{
+    res.setHeader = "Content-Type", "text/html"
 
+    if(req.url == "/" && req.method == "GET"){
+        res.statusCode = 200
+
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Home</title>
+            </head>
+            <body>
+
+            <h1>Home page</h1>
+            <p>Welcome to my simple node.js server</p>
+
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </nav>
+            </body>
+            </html>`)
+    }
+
+    
 })
 
 server.listen(5000, ()=>{
